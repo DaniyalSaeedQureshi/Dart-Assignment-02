ce46437b6f79768bd1dbb2a58ebeda4cfb155813
DaniyalSaeedQureshi <daniyalsaeed4900@gmail.com>
2024-12-14 00:33:44 +0500

    New Name added to name list

--- Q1.dart
+++ Q1.dart
@@ -1,6 +1,6 @@
 // Q.1: Create a list of names and print all names using list.
 
 void main (){
-  List<String> name=["Daniyal","Huraira","Gani","Uzair","Asif","Mhuammad","Mubashir"];
+  List<String> name=["Daniyal","Huraira","Gani","Uzair","Asif","Mhuammad","Mubashir","Saeed"];
   print(name);
 }
